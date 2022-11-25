bf449f9d17e154b6539a0a3cd438a14b1738e47d
Mikalai <45792926+kolyamoloko@users.noreply.github.com>
2022-11-25 13:52:33 +0400

    fix: main socket

--- client/main.js
+++ client/main.js
@@ -5,20 +5,26 @@ pingButton.addEventListener("click", ping);
 const startForm = document.getElementById("startForm");
 const nameInput = document.getElementById("nameInput");
 const codeInput = document.getElementById("codeInput");
+const gameStatus = document.getElementById("gameStatus");
 const socket = io();
+const activeGame = null;
+const gameCode = null;
+const playerNum = null;
+
 startForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const username = nameInput.value;
     const code = codeInput.value;
-    socket.emit("joinGAme", username, code,  () => {
-
+    socket.emit("joinGAme", username, code,  (success, joinedNumber) => {
+        console.log(success);
+        if(success) {
+            playerNum = joinNumber;
+            gameCode = code;
+            activeGame = "TODO";
+            updateHeader();
+        }
     });
-})
-
-
-
-
-
+});
 
 for(let cell of cells){
     cell.addEventListener('click', handleCellClick)
@@ -40,4 +46,9 @@ function ping() {
             console.log(resObj.ping + " - " + (endTime - startTime) + "ms"  )
         })
         .catch(); 
+}
+
+function updateHeader() {
+    if(!activeGame) gameStatus.innerText = "No Active Game!";
+    else if (!activeGame.playerTwoName) gameStatus.innerText = "waiting for an Opponent!";
 }
--- socket.js
+++ socket.js
@@ -7,17 +7,24 @@ export default function startSocketServer(server){
     io.on("connection", (socket) => {
         console.log(socket.id);
         socket.on("joinGame", ( username, code, callback ) => {
+            let playerNum = -1;
             if(activeGames.has(code)){
                 const thisGame = activeGames.get(code);
                 if(!thisGame.player2){
                     callback(false);
                     return;
                 }
+                thisGame.player = new Player(socket.id, username, 2);
+                playerNum = 2;
+            } else {
+                const newGame = new Game(new Player(socket.id, username ),
+                null);
+                activeGames.set(code, newGame);
+                playerNum = 1;
             }
-            const newGame = new Game(new Player(socket.id, username ));
-            activeGames.set(code, newGame);
             socket.join(code);
             io.to(code).emit("updateGame", newGame.currentState);
+            callback(true, playerNum);
         });
     });
 }
