c5be11db19c9a3567415f2270394a15aaf2d93b8
Mikalai <45792926+kolyamoloko@users.noreply.github.com>
2022-11-27 16:29:08 +0400

    fix: some bugs

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ import { startSocketServer } from "./socket.js";
 
 const app = Express();
 const server = http.createServer(app);
-const port = process.env.PORT || 4000;
-
+const port = 4000;
 app.use("/", Express.static("client"));
 app.get("/ping", (req, res) => {
 	res.status(200).send({ ping: "Pong!" });
 });
 
-server.listen(port);
+server.listen(port, () => console.log(`Listening on port ${port}`));
 
 startSocketServer(server);
