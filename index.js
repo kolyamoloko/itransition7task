91e55735a3fe645d20a7f0dd8bbf7b1ae14a15ef
Mikalai <45792926+kolyamoloko@users.noreply.github.com>
2022-11-27 16:18:11 +0400

    prepare: for netlify

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ import { startSocketServer } from "./socket.js";
 const app = Express();
 const server = http.createServer(app);
 const port = 4000;
+
 app.use("/", Express.static("client"));
 app.get("/ping", (req, res) => {
 	res.status(200).send({ ping: "Pong!" });
 });
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(process.env.PORT || 4000);
 
 startSocketServer(server);
